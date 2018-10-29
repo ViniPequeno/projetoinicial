7e60a95e45defece3b24724dd19e14ebe388a6b4
Pedro Pequeno <viniciuspedro350@gmail.com>
2018-10-28 21:03:25 -0300

    Crud

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Cadastro from '@/components/Cadastro'
+import Formulario from '@/components/Formulario'
 import Menu from '@/components/Menu'
 import Listar from '@/components/Listar'
 
@@ -8,23 +8,26 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Menu',
-      component: Menu,
-      children: [
-        {
-          path: '/Listar',
-          name: 'Listar',
-          component: Listar
-        },
-        {
-          path: '/Cadastro',
-          name: 'Cadastro',
-          component: Cadastro,
-        }
-      ]
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Menu',
+    component: Menu,
+    children: [{
+        path: '/Listar',
+        name: 'Listar',
+        component: Listar
+      },
+      {
+        path: '/Cadastro',
+        name: 'Cadastro',
+        component: Formulario,
+      },
+      {
+        props: true,
+        path: '/Alterar/:id',
+        name: 'Alterar',
+        component: Formulario,
+      }
+    ]
+  }]
 })
